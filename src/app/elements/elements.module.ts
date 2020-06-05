a4dd183892b047ecb3383249bc971583f1cde156
renataapmonteiro <60657822+renataapmonteiro@users.noreply.github.com>
2020-06-05 12:29:38 -0300

    second stage

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
 import { ElementsRoutingModule } from './elements-routing.module';
 import { ElementshomeComponent } from './elementshome/elementshome.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
+import { TimesDirective } from './times.directive';
 
 
 @NgModule({
-  declarations: [ElementshomeComponent, PlaceholderComponent],
+  declarations: [ElementshomeComponent, PlaceholderComponent, TimesDirective],
   imports: [
     CommonModule,
-    ElementsRoutingModule
+    ElementsRoutingModule,
+    SharedModule
   ],
   // exports:[]
 })
